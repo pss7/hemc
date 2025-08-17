8200660bc9cdb4b1dac315fff3877c5c45f067a7
parksangseok <park0157@naver.com>
2025-08-17 12:28:26 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -18,11 +18,42 @@ $(function () {
   });
 
   //사이트맵
-  $('#headerWrap .sitemapBtn').click(function(){
+  $('#headerWrap .sitemapBtn').click(function () {
     $('.sitemapBox').fadeIn();
   })
-    $('#headerWrap .sitemapBox .closeBtn').click(function(){
+  $('#headerWrap .sitemapBox .closeBtn').click(function () {
     $('.sitemapBox').fadeOut();
   })
+
+  //모바일메뉴
+  $('#headerWrap .mobileDepht01 > li:has(ul)').children('h2').addClass('arrow');
+  $('#headerWrap .mobileDepht01 li h2 a').click(function () {
+    if ($(this).parent().hasClass('active')) {
+      $('#headerWrap .mobileDepht01 li h2 a').parent().removeClass('active');
+      $('#headerWrap .mobileDepht01 li h2 a').parent().next().slideUp();
+    } else {
+      $('#headerWrap .mobileDepht01 li h2 a').parent().removeClass('active');
+      $('#headerWrap .mobileDepht01 li h2 a').parent().next().slideUp();
+      $(this).parent().addClass('active');
+      $(this).parent().next().slideDown();
+    }
+  });
+
+  $('#headerWrap .mobileMenuBtn').click(function () {
+    $('#headerWrap .mobileMenuWrap').fadeIn(200);
+    $('#headerWrap .mobileMenuBox').animate({ right: 0 }, 250);
+  });
+
+  $('#headerWrap .closeBtn').click(function () {
+    $('#headerWrap .mobileMenuBox').animate({ right: '-100%' }, 250, function () {
+      $('#headerWrap .mobileMenuWrap').fadeOut(200);
+    });
+  });
+
+  $('#headerWrap .mobileMenuWrap').click(function () {
+    $('#headerWrap .mobileMenuBox').animate({ right: '-100%' }, 250, function () {
+      $('#headerWrap .mobileMenuWrap').fadeOut(200);
+    });
+  });
 
 });
